e1563c221ada50bb19e8b7a1a247daaa4bb8d870
15520487-kahdeg <15520487@gm.uit.edu.vn>
2018-01-19 19:57:18 +0700

    Make the whole thing a class library

---
+++ Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilities {
+	public partial class CONTENT_MANAGER  {
+		public static void Log(string data) {
+
+		}
+	}
+}
