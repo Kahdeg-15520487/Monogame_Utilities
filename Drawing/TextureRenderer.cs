61baa31995b3860cfab4eb117d22c9ad7cada6c3
15520487-kahdeg <15520487@gm.uit.edu.vn>
2018-03-13 22:09:12 +0700

    Change TextureRenderer.Render method signature to accept multiple drawDelegate

--- Drawing/TextureRenderer.cs
+++ Drawing/TextureRenderer.cs
@@ -3,83 +3,93 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Utility.Drawing {
-	public static class TextureRenderer {
-		/// <summary>
-		/// Render an array of texture on to a texture
-		/// </summary>
-		/// <param name="size">the normal size of the target texture before apply scaling</param>
-		/// <param name="scale">the scale of the final texture</param>
-		/// <param name="textureList">list of textures and its position on the target texture before apply scaling</param>
-		/// <returns></returns>
-		public static Texture2D Render(SpriteBatch spriteBatch, Vector2 size, float scale = 1f, params Tuple<Vector2, Texture2D>[] textureList) {
-			var graphicsDevice = spriteBatch.GraphicsDevice;
-			RenderTarget2D result = new RenderTarget2D(graphicsDevice, (int)size.X, (int)size.Y);
-			graphicsDevice.SetRenderTarget(result);
-			graphicsDevice.Clear(Color.Black);
+namespace Utility.Drawing
+{
+    public static class TextureRenderer
+    {
+        /// <summary>
+        /// Render an array of texture on to a texture
+        /// </summary>
+        /// <param name="size">the normal size of the target texture before apply scaling</param>
+        /// <param name="scale">the scale of the final texture</param>
+        /// <param name="textureList">list of textures and its position on the target texture before apply scaling</param>
+        /// <returns></returns>
+        public static Texture2D Render(SpriteBatch spriteBatch, Vector2 size, float scale = 1f, params Tuple<Vector2, Texture2D>[] textureList)
+        {
+            var graphicsDevice = spriteBatch.GraphicsDevice;
+            RenderTarget2D result = new RenderTarget2D(graphicsDevice, (int)size.X, (int)size.Y);
+            graphicsDevice.SetRenderTarget(result);
+            graphicsDevice.Clear(Color.Black);
 
-			spriteBatch.Begin();
+            spriteBatch.Begin();
 
-			foreach (var sprite in textureList) {
-				spriteBatch.Draw(sprite.Item2, sprite.Item1, Color.White);
-			}
+            foreach (var sprite in textureList)
+            {
+                spriteBatch.Draw(sprite.Item2, sprite.Item1, Color.White);
+            }
 
-			spriteBatch.End();
-			graphicsDevice.SetRenderTarget(null);
+            spriteBatch.End();
+            graphicsDevice.SetRenderTarget(null);
 
-			if (scale == 1) {
-				return result;
-			}
+            if (scale == 1)
+            {
+                return result;
+            }
 
-			RenderTarget2D finalresult = new RenderTarget2D(graphicsDevice, (int)(size.X / scale), (int)(size.Y / scale));
-			graphicsDevice.SetRenderTarget(finalresult);
-			graphicsDevice.Clear(Color.Black);
-			spriteBatch.Begin();
+            RenderTarget2D finalresult = new RenderTarget2D(graphicsDevice, (int)(size.X / scale), (int)(size.Y / scale));
+            graphicsDevice.SetRenderTarget(finalresult);
+            graphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
 
-			spriteBatch.Draw(result, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+            spriteBatch.Draw(result, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
 
-			spriteBatch.End();
-			graphicsDevice.SetRenderTarget(null);
+            spriteBatch.End();
+            graphicsDevice.SetRenderTarget(null);
 
-			return finalresult;
-		}
+            return finalresult;
+        }
 
-		/// <summary>
-		/// Take a drawer method and draw it on to a texture
-		/// </summary>
-		/// <param name="drawer">the drawer method, only take in one parameter SpriteBatch</param>
-		/// <param name="size">the normal size of the target texture before apply scaling</param>
-		/// <param name="scale">the scale of the final texture</param>
-		/// <returns></returns>
-		public static Texture2D Render(Action<SpriteBatch> drawer, SpriteBatch spriteBatch, Vector2 size, Vector2 offset, Color background, float scale = 1f) {
-			GraphicsDevice graphicsDevice = spriteBatch.GraphicsDevice;
-			RenderTarget2D result = new RenderTarget2D(graphicsDevice, (int)size.X, (int)size.Y);
-			graphicsDevice.SetRenderTarget(result);
-			graphicsDevice.Clear(Color.Black);
+        /// <summary>
+        /// Take a drawer method and draw it on to a texture
+        /// </summary>
+        /// <param name="drawer">the drawer method, only take in one parameter SpriteBatch</param>
+        /// <param name="size">the normal size of the target texture before apply scaling</param>
+        /// <param name="scale">the scale of the final texture</param>
+        /// <returns></returns>
+        public static Texture2D Render(SpriteBatch spriteBatch, Vector2 size, Vector2 offset, Color background, float scale = 1f, params Action<SpriteBatch>[] drawerDelegates)
+        {
+            GraphicsDevice graphicsDevice = spriteBatch.GraphicsDevice;
+            RenderTarget2D result = new RenderTarget2D(graphicsDevice, (int)size.X, (int)size.Y);
+            graphicsDevice.SetRenderTarget(result);
+            graphicsDevice.Clear(Color.Black);
 
-			spriteBatch.Begin();
+            spriteBatch.Begin();
 
-			drawer(spriteBatch);
+            foreach (var dd in drawerDelegates)
+            {
+                dd.Invoke(spriteBatch);
+            }
 
-			spriteBatch.End();
-			graphicsDevice.SetRenderTarget(null);
+            spriteBatch.End();
+            graphicsDevice.SetRenderTarget(null);
 
 
-			if (scale == 1) {
-				return result;
-			}
+            if (scale == 1)
+            {
+                return result;
+            }
 
-			RenderTarget2D finalresult = new RenderTarget2D(graphicsDevice, (int)(size.X / scale), (int)(size.Y / scale));
-			graphicsDevice.SetRenderTarget(finalresult);
-			graphicsDevice.Clear(Color.Black);
-			spriteBatch.Begin();
+            RenderTarget2D finalresult = new RenderTarget2D(graphicsDevice, (int)(size.X / scale), (int)(size.Y / scale));
+            graphicsDevice.SetRenderTarget(finalresult);
+            graphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
 
-			spriteBatch.Draw(result, offset, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+            spriteBatch.Draw(result, offset, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
 
-			spriteBatch.End();
-			graphicsDevice.SetRenderTarget(null);
+            spriteBatch.End();
+            graphicsDevice.SetRenderTarget(null);
 
-			return finalresult;
-		}
-	}
+            return finalresult;
+        }
+    }
 }
--- UI/MessageBox.cs
+++ UI/MessageBox.cs
@@ -4,137 +4,161 @@ using Microsoft.Xna.Framework.Input;
 using System;
 using Utility.Drawing;
 
-namespace Utility.UI {
-	public class MessageBox : UIObject {
-		Button middleButton;
-		Button leftButton;
-		Button rightButton;
-		Label label;
-		private Texture2D background;
+namespace Utility.UI
+{
+    public class MessageBox : UIObject
+    {
+        Button middleButton;
+        Button leftButton;
+        Button rightButton;
+        Label label;
+        private Texture2D background;
 
-		public override float Depth {
-			get => depth;
-			set {
-				depth = value;
-				middleButton.Depth = depth;
-				label.Depth = depth;
-			}
-		}
+        public override float Depth
+        {
+            get => depth;
+            set
+            {
+                depth = value;
+                middleButton.Depth = depth;
+                label.Depth = depth;
+            }
+        }
 
-		private string defaultPrompt;
-		private string defaultButtonText;
-		
-		public virtual Keys MiddleButtonHotkey { get => middleButton.Hotkey; set=>middleButton.Hotkey = value; }
-		public virtual Keys LeftButtonHotkey { get => leftButton.Hotkey; set => leftButton.Hotkey = value; }
-		public virtual Keys RightButtonHotkey { get => rightButton.Hotkey; set => rightButton.Hotkey = value; }
+        private string defaultPrompt;
+        private string defaultButtonText;
 
-		public MessageBox(Point position, string prompt = null, string buttontext = null) {
-			//the msgbox is gonna be 240 x 100
-			Position = position;
+        public virtual Keys MiddleButtonHotkey { get => middleButton.Hotkey; set => middleButton.Hotkey = value; }
+        public virtual Keys LeftButtonHotkey { get => leftButton.Hotkey; set => leftButton.Hotkey = value; }
+        public virtual Keys RightButtonHotkey { get => rightButton.Hotkey; set => rightButton.Hotkey = value; }
 
-			defaultPrompt = prompt ?? string.Empty;
-			defaultButtonText = buttontext ?? string.Empty;
+        public MessageBox(Point position, string prompt = null, string buttontext = null)
+        {
+            //the msgbox is gonna be 240 x 100
+            Position = position;
 
-			//the button gonna be 80x30
-			middleButton = new Button(defaultButtonText, new Point(position.X + 80, position.Y + 100), new Vector2(80, 30), CONTENT_MANAGER.Fonts["default"]) {
-				Origin = new Vector2(0, 0)
-			};
-			leftButton = new Button(defaultButtonText, new Point(position.X, position.Y + 100), new Vector2(80, 30), CONTENT_MANAGER.Fonts["default"]) {
-				Origin = new Vector2(13, 0),
-				IsVisible = false
-			};
-			rightButton = new Button(defaultButtonText, new Point(position.X + 160, position.Y + 100), new Vector2(80, 30), CONTENT_MANAGER.Fonts["default"]) {
-				Origin = new Vector2(10, 0),
-				IsVisible = false
-			};
+            defaultPrompt = prompt ?? string.Empty;
+            defaultButtonText = buttontext ?? string.Empty;
 
-			label = new Label(defaultPrompt, position, new Vector2(50, 30), CONTENT_MANAGER.Fonts["default"], 1f);
-			background = TextureRenderer.Render(Primitive2DActionGenerator.FillRectangle(new Rectangle(Point.Zero, new Point(240, 100)), Color.LightGray), CONTENT_MANAGER.spriteBatch, new Vector2(240, 100), Vector2.Zero, Color.White);
+            //the button gonna be 80x30
+            middleButton = new Button(defaultButtonText, new Point(position.X + 80, position.Y + 100), new Vector2(80, 30), CONTENT_MANAGER.Fonts["default"])
+            {
+                Origin = new Vector2(0, 0)
+            };
+            leftButton = new Button(defaultButtonText, new Point(position.X, position.Y + 100), new Vector2(80, 30), CONTENT_MANAGER.Fonts["default"])
+            {
+                Origin = new Vector2(13, 0),
+                IsVisible = false
+            };
+            rightButton = new Button(defaultButtonText, new Point(position.X + 160, position.Y + 100), new Vector2(80, 30), CONTENT_MANAGER.Fonts["default"])
+            {
+                Origin = new Vector2(10, 0),
+                IsVisible = false
+            };
 
-			depth = LayerDepth.GuiBackground;
-			middleButton.Depth = LayerDepth.GuiLower;
-			label.Depth = LayerDepth.GuiLower;
+            label = new Label(defaultPrompt, position, new Vector2(50, 30), CONTENT_MANAGER.Fonts["default"], 1f);
+            background = TextureRenderer.Render(CONTENT_MANAGER.spriteBatch, new Vector2(240, 100), Vector2.Zero, Color.White, 1, Primitive2DActionGenerator.FillRectangle(new Rectangle(Point.Zero, new Point(240, 100)), Color.LightGray));
 
-			middleButton.MouseClick += (o, e) => MiddleButtonPressed?.Invoke(this, e);
-			leftButton.MouseClick += (o, e) => LeftButtonPressed?.Invoke(this, e);
-			rightButton.MouseClick += (o, e) => RightButtonPressed?.Invoke(this, e);
-		}
+            depth = LayerDepth.GuiBackground;
+            middleButton.Depth = LayerDepth.GuiLower;
+            label.Depth = LayerDepth.GuiLower;
 
-		public void Show(string prompt = null, string buttontext = null, string leftbtn = null, string rightbtn = null) {
-			label.Text = prompt ?? defaultPrompt;
-			middleButton.Text = buttontext ?? defaultButtonText;
+            middleButton.MouseClick += (o, e) => MiddleButtonPressed?.Invoke(this, e);
+            leftButton.MouseClick += (o, e) => LeftButtonPressed?.Invoke(this, e);
+            rightButton.MouseClick += (o, e) => RightButtonPressed?.Invoke(this, e);
+        }
 
-			if (!string.IsNullOrEmpty(leftbtn)) {
-				leftButton.IsVisible = true;
-				leftButton.Text = leftbtn;
-			}
-			else {
-				leftButton.IsVisible = false;
-			}
-			if (!string.IsNullOrEmpty(rightbtn)) {
-				rightButton.IsVisible = true;
-				rightButton.Text = rightbtn;
-			}
-			else {
-				rightButton.IsVisible = false;
-			}
+        public void Show(string prompt = null, string buttontext = null, string leftbtn = null, string rightbtn = null)
+        {
+            label.Text = prompt ?? defaultPrompt;
+            middleButton.Text = buttontext ?? defaultButtonText;
 
-			isVisible = true;
-			middleButton.IsVisible = IsVisible;
-			label.IsVisible = IsVisible;
-		}
+            if (!string.IsNullOrEmpty(leftbtn))
+            {
+                leftButton.IsVisible = true;
+                leftButton.Text = leftbtn;
+            }
+            else
+            {
+                leftButton.IsVisible = false;
+            }
+            if (!string.IsNullOrEmpty(rightbtn))
+            {
+                rightButton.IsVisible = true;
+                rightButton.Text = rightbtn;
+            }
+            else
+            {
+                rightButton.IsVisible = false;
+            }
 
-		public void Hide() {
-			isVisible = false;
-			middleButton.IsVisible = IsVisible;
-			label.IsVisible = IsVisible;
+            isVisible = true;
+            middleButton.IsVisible = IsVisible;
+            label.IsVisible = IsVisible;
+        }
 
-			leftButton.IsVisible = false;
-			rightButton.IsVisible = false;
-		}
+        public void Hide()
+        {
+            isVisible = false;
+            middleButton.IsVisible = IsVisible;
+            label.IsVisible = IsVisible;
 
-		public override void Update(GameTime gameTime, InputState inputState, InputState lastInputState) {
-			middleButton.Update(gameTime, inputState, lastInputState);
-			if (leftButton.IsVisible) {
-				leftButton.Update(gameTime, inputState, lastInputState);
-			}
-			if (rightButton.IsVisible) {
-				rightButton.Update(gameTime, inputState, lastInputState);
-			}
-			label.Update(gameTime, inputState, lastInputState);
-		}
+            leftButton.IsVisible = false;
+            rightButton.IsVisible = false;
+        }
 
-		public override void Draw(SpriteBatch spriteBatch, GameTime gameTime) {
-			spriteBatch.Draw(background, new Rectangle(Position, new Point(240, 100)), null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, Depth);
-			middleButton.Draw(spriteBatch, gameTime);
-			label.Draw(spriteBatch, gameTime);
+        public override void Update(GameTime gameTime, InputState inputState, InputState lastInputState)
+        {
+            middleButton.Update(gameTime, inputState, lastInputState);
+            if (leftButton.IsVisible)
+            {
+                leftButton.Update(gameTime, inputState, lastInputState);
+            }
+            if (rightButton.IsVisible)
+            {
+                rightButton.Update(gameTime, inputState, lastInputState);
+            }
+            label.Update(gameTime, inputState, lastInputState);
+        }
 
-			if (leftButton.IsVisible) {
-				leftButton.Draw(spriteBatch, gameTime);
-			}
-			if (rightButton.IsVisible) {
-				rightButton.Draw(spriteBatch, gameTime);
-			}
-			//draw the border
-			//draw the prompt
-			//draw button
-		}
+        public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.Draw(background, new Rectangle(Position, new Point(240, 100)), null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, Depth);
+            middleButton.Draw(spriteBatch, gameTime);
+            label.Draw(spriteBatch, gameTime);
 
-		public event EventHandler<UIEventArgs> MiddleButtonPressed;
-		public event EventHandler<UIEventArgs> LeftButtonPressed;
-		public event EventHandler<UIEventArgs> RightButtonPressed;
-		public event EventHandler<UIEventArgs> MessageboxClicked;
-		protected virtual void OnButtonPressed(object sender, UIEventArgs e) {
-			MiddleButtonPressed?.Invoke(sender, e);
-		}
-		protected virtual void OnLeftButtonPressed(object sender, UIEventArgs e) {
-			LeftButtonPressed?.Invoke(sender, e);
-		}
-		protected virtual void OnRightButtonPressed(object sender, UIEventArgs e) {
-			RightButtonPressed?.Invoke(sender, e);
-		}
-		protected virtual void OnMessageboxClicked(object sender, UIEventArgs e) {
-			MessageboxClicked?.Invoke(sender, e);
-		}
-	}
+            if (leftButton.IsVisible)
+            {
+                leftButton.Draw(spriteBatch, gameTime);
+            }
+            if (rightButton.IsVisible)
+            {
+                rightButton.Draw(spriteBatch, gameTime);
+            }
+            //draw the border
+            //draw the prompt
+            //draw button
+        }
+
+        public event EventHandler<UIEventArgs> MiddleButtonPressed;
+        public event EventHandler<UIEventArgs> LeftButtonPressed;
+        public event EventHandler<UIEventArgs> RightButtonPressed;
+        public event EventHandler<UIEventArgs> MessageboxClicked;
+        protected virtual void OnButtonPressed(object sender, UIEventArgs e)
+        {
+            MiddleButtonPressed?.Invoke(sender, e);
+        }
+        protected virtual void OnLeftButtonPressed(object sender, UIEventArgs e)
+        {
+            LeftButtonPressed?.Invoke(sender, e);
+        }
+        protected virtual void OnRightButtonPressed(object sender, UIEventArgs e)
+        {
+            RightButtonPressed?.Invoke(sender, e);
+        }
+        protected virtual void OnMessageboxClicked(object sender, UIEventArgs e)
+        {
+            MessageboxClicked?.Invoke(sender, e);
+        }
+    }
 }
